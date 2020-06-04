d6238fb606a0de2769a999891f103eb020f67457
Mazid Ahmad <mazid.ahmad.tif17@polban.ac.id>
2020-06-04 19:24:57 +0700

    Revert "Refactoring"
    This reverts commit 89d37b1fe72d5c52e369fbb4ee63e557c52b247d.

--- lib/src/models/userModels.dart
+++ lib/src/models/userModels.dart
@@ -1,3 +1,6 @@
+import 'package:equatable/equatable.dart';
+import 'dart:convert';
+
 class User {
 
   String id_user;
@@ -9,21 +12,32 @@ class User {
   String photo_user;
   String status;
 
-  User(this.id_user, this.name_user, this.email_user, this.username,
-      this.password_user,
-      this.phone_user, this.photo_user, this.status);
+  @override
+  List<Object> get props => [id_user, name_user, email_user, password_user,
+    phone_user, photo_user, status];
 
-  User.fromJason(Map<String, dynamic> json)
-    : id_user = json['id_user'],
-    name_user = json['name_user'],
-    email_user = json['email_user'],
-    username = json['username'],
-    password_user = json['password_user'],
-    phone_user = json['password_user'],
-    photo_user = json['photo_user'];
-
-  SetNameUser(String name){
-    this.name_user = name;
-  }
+//  static User fromJson(dynamic json) {
+//    return User(
+//      id_user: json['id_user'],
+//      name_user: json['name_user'],
+//      email_user: json['email_user'],
+//      username: json['username'],
+//      password_user: json['password_user'],
+//      phone_user: json['password_user'],
+//      photo_user: json['photo_user']
+//    );
+//  }
+//
+//  @override
+//  String toString() => 'User { id_user: $id_user }';
 }
-
+//
+//List<User> userFromJson(String str){
+//  final jsonData = json.decode(str);
+//  return new List<User>.from(jsonData.map((x) => User.fromJson(x)));
+//}
+//
+//String userToJson(List<User> data){
+//  final dyn = new List<dynamic>.from(data.map((x) => x.userToJson()));
+//  return json.encode(dyn);
+//}
