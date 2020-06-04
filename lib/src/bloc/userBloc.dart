cf2af6febdf3c1bcd5e6a17628c13fdad5b1db0d
Mazid Ahmad <mazid.ahmad.tif17@polban.ac.id>
2020-06-04 19:28:47 +0700

    Refactor User

--- lib/src/bloc/userBloc.dart
+++ lib/src/bloc/userBloc.dart
@@ -1,10 +1,9 @@
 import 'package:apos/src/models/userModels.dart';
-import 'package:apos/src/resources/repository.dart';
+import 'package:apos/src/resources/userRepository.dart';
 import 'package:rxdart/rxdart.dart';
 
 class UserBloc {
-  final _repository = Repository();
-  final _userFetcher = PublishSubject<List<User>>();
+  final _userRepository = UserRepository();
 
   final _name_user = BehaviorSubject<String>();
   final _email_user = BehaviorSubject<String>();
@@ -13,27 +12,20 @@ class UserBloc {
   final _phone_user = BehaviorSubject<String>();
 //  final _photo_user = BehaviorSubject<String>();
 
-  Stream<List<User>> get AllUser => _userFetcher.stream;
-
   Function(String) get name_user => _name_user.sink.add;
   Function(String) get email_user => _email_user.sink.add;
   Function(String) get username_user => _username.sink.add;
   Function(String) get password_user => _password_user.sink.add;
   Function(String) get phone_user => _phone_user.sink.add;
 
-//  fetchAllUser() async{
-//    List<User> users = await _repository.fetchAllUser();
-//    _userFetcher.sink.add(users);
-//  }
-
   registerUser() async {
-    await _repository.registerUser(_name_user.value, _email_user.value,
-      _username.value, _password_user.value, _phone_user.value);
-    
+    User user = User(_name_user.value, _email_user.value,
+        _username.value, _password_user.value, _phone_user.value,"");
+    await _userRepository.registerUser(user);
+
   }
 
   dispose(){
-    _userFetcher.close();
     _name_user.close();
     _email_user.close();
     _username.close();
--- lib/src/resources/userProvider.dart
+++ lib/src/resources/userProvider.dart
@@ -6,23 +6,14 @@ class UserProvider {
   Client client =  Client();
   final _url = "https://apos-server.herokuapp.com";
 
-//  Future<List<User>> fetchUserList() async{
-//    final response = await client.get(_url);
-//    if(response.statusCode == 200){
-//      return compute(userFromJson, response.body);
-//    } else {
-//      throw Exception('Failed to load');
-//    }
-//  }
-
-  Future registerUser(name_user, email_user, username, password_user, phone_user) async {
+  Future registerUser(User user) async {
     final response = await client.post("$_url/auth/register", body: {
-      'name_user' : name_user,
-      'email_user' : email_user,
-      'username' : username,
-      'password_user' : password_user,
-      'phone_user' : phone_user,
-//      'photo_user' : photo_user;
+      'name_user' : user.name_user,
+      'email_user' : user.email_user,
+      'username' : user.username,
+      'password_user' : user.password_user,
+      'phone_user' : user.phone_user,
+//      'photo_user' : user.photo_user;
     });
 
     if(response.statusCode == 201){
