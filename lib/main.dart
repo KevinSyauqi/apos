ea27558b0c9ed51634b0c271635adc9a5a9c09ac
Mazid Ahmad <mazid.ahmad.tif17@polban.ac.id>
2020-06-05 08:38:15 +0700

    Refactor Login + Logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
+import 'package:apos/src/bloc/authentication/authenticationBloc.dart';
+import 'package:apos/src/bloc/authentication/authenticationEvent.dart';
+import 'package:apos/src/resources/authenticationRepository.dart';
 import 'package:apos/src/ui/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:apos/src/ui/login_page.dart';
 
-void main() => runApp(Apos());
+void main() =>
+    runApp(
+        RepositoryProvider<AuthenticationRepository>(
+          create: (context) {
+            return FakeAuthenticationRepository();
+          },
+          child: BlocProvider<AuthenticationBloc>(
+            create: (context) {
+              final authRepository = RepositoryProvider.of<
+                  AuthenticationRepository>(context);
+              return AuthenticationBloc(authRepository)..add(AppLoaded());
+            },
+            child: Apos(),
+          ),
+        )
+    );
 
 class Apos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SplashPage(),
+      home: SplashPage()
     );
   }
 }
---
+++ lib/src/resources/authenticationRepository.dart
@@ -0,0 +1,29 @@
+import '../models/models.dart';
+
+abstract class AuthenticationRepository {
+  Future<User> getCurrentUser();
+  Future<User> signInWithEmailAndPassword(String email, String password);
+  Future<void> signOut();
+}
+
+class FakeAuthenticationRepository extends AuthenticationRepository {
+  @override
+  Future<User> getCurrentUser() async {
+    return null; // return null for now
+  }
+
+  @override
+  Future<User> signInWithEmailAndPassword(String email, String password) async {
+    await Future.delayed(Duration(seconds: 1)); // simulate a network delay
+
+    if (email.toLowerCase() != 'admin@apos.com' || password != 'admin') {
+      throw Exception('Wrong username or password');
+    }
+    return User("Test User", email, "Test", password, "086732537232", "");
+  }
+
+  @override
+  Future<void> signOut() {
+    return null;
+  }
+}
